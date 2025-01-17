6e53c9c920a36d3a0983eb1c8c5ca90199ab339e
Давид Химич <1412frodo@gmail.com>
2025-01-17 22:27:37 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def hello():
     name = request.args.get("name", "Recruto")
     message = request.args.get("message", "Давай дружить")
     return f"Hello {name}! {message}!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))  # Используем порт из переменной окружения
+    app.run(host="0.0.0.0", port=port)
